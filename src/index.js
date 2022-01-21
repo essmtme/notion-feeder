07a6a65c6354075f4ff98f92bdb9b1e49620c154
essmtme <97328090+essmtme@users.noreply.github.com>
2022-01-21 15:19:04 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,9 +15,11 @@ async function index() {
       link: item.link,
       content: htmlToNotionBlocks(item.content),
     };
+    const x = 'found a item';
+    console.log(x);
     await addFeedItemToNotion(notionItem);
   }
-
+  await deleteOldUnreadFeedItemsFromNotion();
 }
 
 index();
