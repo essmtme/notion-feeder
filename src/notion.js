9eabc805ec5af8a8823941d35635e1984a452004
essmtme <97328090+essmtme@users.noreply.github.com>
2022-01-21 14:47:23 -0500

    Update notion.js

--- src/notion.js
+++ src/notion.js
@@ -86,12 +86,12 @@ export async function deleteOldUnreadFeedItemsFromNotion() {
     logLevel,
   });
 
-  // Create a datetime which is 3000 days earlier than the current time
+  // Create a datetime which is 2 days earlier than the current time
   const fetchBeforeDate = new Date();
-  fetchBeforeDate.setDate(fetchBeforeDate.getDate() - 3000);
+  fetchBeforeDate.setDate(fetchBeforeDate.getDate() - 2);
 
   // Query the feed reader database
-  // and fetch only those items that are unread or created before last 3000 days
+  // and fetch only those items that are unread or created before last 2 days
   let response;
   try {
     response = await notion.databases.query({
